91217bb06e245d24ff0e68c3357fce12cbef4235
kevin-cmd99 <heypeople60@gmail.com>
2024-10-02 14:41:05 -0400

    light editing

---
+++ Week-1/Day-3-Java/Java-Continued/exceptions/ExceptionBasics.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/*
+  Exceptions are Java way to telling you something has gone wring diuring runtime
+    - Error: wrong during compiling part of code
+    - Exception: something that goes wrong during the code executiion (source code copiled)
+ */
+
+public class ExceptionBasics {
+    /*
+     * Two primary exceptions, Checked and unchecked
+     */
+
+     public static void main(String[] args) {
+        ExceptionBasics
+
+     }
+}
